a637564b8d62224973cf9a5ee69f37f59e0f354f
Moe <mohamad.yeh@gmail.com>
2020-08-15 22:55:17 -0400

    Add perfect number function

--- Beginner.py
+++ Beginner.py
@@ -1,4 +1,4 @@
-def palindrome():
+def palindrom():
     isPalindrome = False
     while not isPalindrome:
         name = input("Enter a word and see if it's a Palindrome\n\n")
@@ -9,4 +9,31 @@ def palindrome():
         else:
             print("Not Palindrome!!!!!!!")
 
-palindrome()
+
+def isPerfectNumber():
+    perfectNumber = input("Please enter a perfect number\n\n")
+    lengthOfPerfectNumber = perfectNumber.__len__()
+    additionOfPerfectNumber = 0
+    for x in perfectNumber:
+        additionOfPerfectNumber += int(x)
+    result = additionOfPerfectNumber ** lengthOfPerfectNumber
+
+    if (result == int(perfectNumber)):
+        print ("This number is perfect")
+
+
+def isNombreParfait():
+    nombreParfait = int(input ("Enter a perfect number\n"))
+    somme = 0
+    for x in range(1, nombreParfait):
+        if (nombreParfait % x == 0):
+            somme += x
+    if (somme == nombreParfait):
+        print ("Le nombre est parfait!!!!")
+    else:
+        print("Le nombre saisi n'est pas parfait!!!!")
+
+
+##isNombreParfait()
+isPerfectNumber()
+##palindrom()
