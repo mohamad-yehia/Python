b2915a87ebd9e75c2ae2661889d871c5df10d6d9
Moe <mohamad.yeh@gmail.com>
2020-08-15 21:12:23 -0400

    first python function

--- Beginner.py
+++ Beginner.py
@@ -1,9 +1,12 @@
-isPalindrome = False
-while not isPalindrome:
-    name = input("Enter a word and see if it's a Palindrome\n\n")
-    nameReversed = name[name.__sizeof__()::-1]
-    if name != "" and name == nameReversed:
-        isPalindrome = True
-        print("Palindrome!!!!!!!!!!!!!!!!!!")
-    else:
-        print("Not Palindrome!!!!!!!!!!!!!!")
+def palindrome():
+    isPalindrome = False
+    while not isPalindrome:
+        name = input("Enter a word and see if it's a Palindrome\n\n")
+        nameReversed = name[name.__sizeof__()::-1]
+        if name != "" and name == nameReversed:
+            isPalindrome = True
+            print("Palindrome!!!!!!!!!!!!!!!!!!")
+        else:
+            print("Not Palindrome!!!!!!!!!!!!!!")
+
+palindrome()
